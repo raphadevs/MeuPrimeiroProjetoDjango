04238e77f32282e836041975f5cee2878af9e0b0
Raphadevs <raphaelcarvalho.law@gmail.com>
2022-07-11 20:33:47 -0300

    Versão inicial do projeto

---
+++ Rh/--SQLite.sql
@@ -0,0 +1,13 @@
+--SQLite
+--SQL (Sctructured Query Languagem)
+--MDL (Manipulate Data Language)
+
+--Seleciona todos os dados da tabela
+SELECT * FROM Rh_departamento;
+
+--insere registro na tabela
+INSERT INTO Rh_departamento (id,nome)
+VALUES (6,'RH');
+
+INSERT INTO Rh_departamento (id, nome)
+VALUES (7, 'Produção);
---
+++ Rh/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+# Create your models here.
+class Departamento(models.Model):
+    nome = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nome
+        
+class Funcionario(models.Model):
+    CARGOS = [
+        ('ES', 'Estagiário'),
+        ('AS', 'Assistente'),
+        ('JR', 'Junior'),
+        ('PL', 'Pleno'),
+        ('SR', 'Sênior'),
+        ('GR', 'Gerente'),
+        ('CEO','Presidente')
+    ]
+    SEXO = [
+        ('F', 'Feminino'),
+        ('M', 'Masculino')
+
+    ]
+    
+    matricula = models.IntegerField()
+    nome = models.CharField(max_length=50)
+    cargo = models.CharField(max_length=3, choices=CARGOS)
+    sexo = models.CharField(max_length=1,choices=SEXO, null=True)
+    departamento  = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+    salario  = models.DecimalField(max_digits=10, decimal_places=2)
+    data_nascimento = models.DateField(null=True)
+
